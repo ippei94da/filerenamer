3520366a0160136d10ce719a789313d8d5f14e36
ippei94da <ippei94da@gmail.com>
2013-11-22 00:36:52 +0900

    daily change

--- lib/filerenamer.rb
+++ lib/filerenamer.rb
@@ -1,16 +1,16 @@
 require "fileutils"
 require "filerenamer/commander.rb"
 require "filerenamer/optionparser.rb"
-require "filerenamer/manipulation.rb"
-require "filerenamer/manipulation/mv.rb"
-require "filerenamer/manipulation/cp.rb"
-require "filerenamer/manipulation/ln.rb"
-require "filerenamer/manipulation/lns.rb"
-#require "filerenamer/manipulation/rm.rb"
-require "filerenamer/manipulationqueue.rb"
-#require "filerenamer/manipulation/mkdirp.rb" not prepare due to ambiguous behavior.
-#require "filerenamer/manipulation/rmdirp.rb" not prepare due to ambiguous behavior.
-require "filerenamer/filetreesimulator.rb"
-require "filerenamer/filetreesimulator/file.rb"
+#require "filerenamer/manipulation.rb"
+#require "filerenamer/manipulation/mv.rb"
+#require "filerenamer/manipulation/cp.rb"
+#require "filerenamer/manipulation/ln.rb"
+#require "filerenamer/manipulation/lns.rb"
+##require "filerenamer/manipulation/rm.rb"
+#require "filerenamer/manipulationqueue.rb"
+##require "filerenamer/manipulation/mkdirp.rb" not prepare due to ambiguous behavior.
+##require "filerenamer/manipulation/rmdirp.rb" not prepare due to ambiguous behavior.
+#require "filerenamer/filetreesimulator.rb"
+#require "filerenamer/filetreesimulator/file.rb"
 
 module FileRenamer; end
